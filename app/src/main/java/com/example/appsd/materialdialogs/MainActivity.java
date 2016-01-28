a5878c5dc37fee049e856e88a3a97645ca451bcd
Md Bulbul Hossen <info247.bulbul@gmail.com>
2016-01-28 12:25:21 +0600

    Back button exit with confirmdialog

--- app/src/main/java/com/example/appsd/materialdialogs/MainActivity.java
+++ app/src/main/java/com/example/appsd/materialdialogs/MainActivity.java
@@ -1,40 +1,43 @@
 package com.example.appsd.materialdialogs;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.github.vignesh_iopex.confirmdialog.Confirm;
 import com.github.vignesh_iopex.confirmdialog.Dialog;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        Button bt = (Button) findViewById(R.id.button);
-        bt.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onBackPressed() {
+        // TODO Auto-generated method stub
+        //super.onBackPressed();
+        openQuitDialog();
+    }
+
+    private void openQuitDialog() {
+        // super.onCreate(savedInstanceState);
+        Confirm.using(this).ask("Are you exit application ?").onPositive("Yes", new Dialog.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-
-                Confirm.using(MainActivity.this).ask("Fire missles?").onPositive("Yes", new Dialog.OnClickListener() {
-                    @Override
-                    public void onClick(Dialog dialog, int which) {
-                        /*launchMissles();*/finish();
-                    }
-                }).onNegative("No", new Dialog.OnClickListener() {
-                    @Override
-                    public void onClick(Dialog dialog, int which) {
-                       // sendFalseAlarm();
-                    }
-                }).build().show();
+            public void onClick(Dialog dialog, int which) {
+                finish();
             }
-
-
-        });
+        }).onNegative("No", new Dialog.OnClickListener() {
+            @Override
+            public void onClick(Dialog dialog, int which) {
+                //sendFalseAlarm();
+            }
+        }).build().show();
     }
 }
